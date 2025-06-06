e02e4d8edfa6960efe528672794108320b5f4bd7
xiaomei001 <541779717@qq.com>
2025-06-06 22:01:57 +0800

    Create 0001az.sh

---
+++ jiaoben/0001az.sh
@@ -0,0 +1,6 @@
+dpkg -i \
+  /tmp/wky-main/pptpvpn/libpcap0.8_1.10.1-4ubuntu1.22.04.1_armhf.deb \
+  /tmp/wky-main/pptpvpn/libpam0g_1.4.0-11ubuntu2.5_armhf.deb \
+  /tmp/wky-main/pptpvpn/libpam-modules_1.4.0-11ubuntu2.5_armhf.deb \
+  /tmp/wky-main/pptpvpn/ppp_2.4.9-1+1ubuntu3_armhf.deb \
+  /tmp/wky-main/pptpvpn/pptp-linux_1.10.0-1build3_armhf.deb
