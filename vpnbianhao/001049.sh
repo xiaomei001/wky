91713a9626a2cc3f22c302a5d536eb3b0cf9d168
xiaomei001 <541779717@qq.com>
2025-06-06 19:59:14 +0800

    Update 001049.sh

--- vpnbianhao/001049.sh
+++ vpnbianhao/001049.sh
@@ -2,7 +2,7 @@
 set -e
 SCRIPT_PATH="/tmp/wky-main/pptpvpn/02install_pptp.sh"       #脚本地址
 #执行命令
-EXEC_CMD="sudo bash $SCRIPT_PATH --vpn 58.22.63.26 --user cong049 --pass 123 --name myvpn --local-net 192.168.98.0/24 --remote-net 192.168.48.0/24"
+EXEC_CMD="sudo bash $SCRIPT_PATH --vpn 58.22.63.216 --user cong049 --pass 123 --name myvpn --local-net 192.168.98.0/24 --remote-net 192.168.48.0/24"
 if [[ -f "$SCRIPT_PATH" ]]; then
   echo "运行脚本: $EXEC_CMD"
   chmod +x "$SCRIPT_PATH"
