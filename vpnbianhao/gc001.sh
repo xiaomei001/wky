8de428483f5f87f93988ab846e137a898cda32cb
xiaomei001 <541779717@qq.com>
2025-06-10 22:58:50 +0800

    Add files via upload

---
+++ vpnbianhao/gc001.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+SCRIPT_PATH="/tmp/wky-main/pptpvpn/02install_pptp.sh"       #脚本地址
+#执行命令
+EXEC_CMD="sudo bash $SCRIPT_PATH --vpn hongguancong.eicp.net --user wky9901 --pass 123 --name myvpn --local-net 192.168.98.0/24 --remote-net 192.168.10.0/24"
+if [[ -f "$SCRIPT_PATH" ]]; then
+  echo "运行脚本: $EXEC_CMD"
+  chmod +x "$SCRIPT_PATH"
+  eval "$EXEC_CMD"
+else
+  echo "未找到脚本: $SCRIPT_PATH"
+  exit 1
+fi
